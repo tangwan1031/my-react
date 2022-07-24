1fb9fdd67cece3e9335e03e8cbf0dfe3fcaec3e8
tangwan <leelhef@126.com>
2022-07-24 22:57:53 +0800

    antd

--- my-react/src/App.tsx
+++ my-react/src/App.tsx
@@ -1,7 +1,14 @@
+import {Button} from "antd";
 import "@/App.less";
+const App: React.FC = () => (
+  <>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </>
+);
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
-}
+export default App;
